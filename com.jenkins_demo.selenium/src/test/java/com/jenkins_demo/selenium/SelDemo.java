3d816b319041901175f65951a26b89226d5a0af9
tejaswiyd <tejaswiyd@192.168.43.234>
2021-03-11 15:32:37 +0530

    First Commit

---
+++ com.jenkins_demo.selenium/src/test/java/com/jenkins_demo/selenium/SelDemo.java
@@ -0,0 +1,13 @@
+package com.jenkins_demo.selenium;
+
+import org.testng.annotations.Test;
+
+
+public class SelDemo {
+	@Test
+	public void printSel()
+	{
+		System.out.println("This is a selenium demo");
+	}
+
+}
